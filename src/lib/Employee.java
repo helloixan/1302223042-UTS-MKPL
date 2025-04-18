1db20687c1a6d0a2028293373a0bb22fd47bed34
IksanRisandy <iksanrisandy@gmail.com>
2025-04-18 21:38:30 +0700

    MERGE PrimitiveObsession-ClassEmployee

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -5,8 +5,13 @@ import java.time.Month;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Employee {
 
+
+public class Employee {
+	public enum Gender {
+		MALE,
+		FEMALE
+	}
 	private String employeeId;
 	private String firstName;
 	private String lastName;
@@ -19,7 +24,7 @@ public class Employee {
 	private int monthWorkingInYear;
 	
 	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
+	private Gender gender; //true = Laki-laki, false = Perempuan
 	
 	private int monthlySalary;
 	private int otherMonthlyIncome;
@@ -31,7 +36,7 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, Gender gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
